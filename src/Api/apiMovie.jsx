d9aed083559322f2473095964d8f64e6c4e3e92a
Mart <marta.nikiel@yahoo.com>
2023-12-05 10:02:25 +0100

    in progress

--- src/Api/apiMovie.jsx
+++ src/Api/apiMovie.jsx
@@ -1,4 +1,9 @@
 import axios from 'axios';
 
 axios.defaults.baseURL = 'https://api.themoviedb.org/3';
-const API_KEY = '';
+const API_KEY = '59f1336664fef9f174e58fbaa28590e8';
+
+// curl --request GET \
+//      --url 'https://api.themoviedb.org/3/trending/movie/day?language=en-US' \
+//      --header 'Authorization: Bearer 59f1336664fef9f174e58fbaa28590e8' \
+//      --header 'accept: application/json'
