7bb22b0e2a64b9d2c7870fd39cf51eb282ebb3a7
Mart <marta.nikiel@yahoo.com>
2023-12-07 12:00:27 +0100

    changed api requests

--- src/utils/Api/apiMovie.jsx
+++ src/utils/Api/apiMovie.jsx
@@ -27,14 +27,14 @@ export const searchMovies = async inputValue => {
 
 export const getMovieDetails = async movieId => {
   const response = await axios.get(
-    `/movie/${movieId}${API_KEY}&language=en-US`
+    `/movie/${movieId}?api_key=${API_KEY}&language=en-US`
   );
   return response.data;
 };
 
 export const getMovieCredits = async moviesId => {
   const response = await axios.get(
-    `/movie/${moviesId}/credits${API_KEY}&language=en-US`
+    `/movie/${moviesId}/credits?api_key=${API_KEY}&language=en-US`
   );
 
   return response.data.cast.map(({ name, character, profile_path, id }) => {
@@ -49,7 +49,7 @@ export const getMovieCredits = async moviesId => {
 
 export const getMoviesReviews = async moviesId => {
   const response = await axios.get(
-    `/movie/${moviesId}/reviews${API_KEY}&language=en-US&page=1`
+    `/movie/${moviesId}/reviews?api_key=${API_KEY}&language=en-US&page=1`
   );
   return response.data.results.map(({ author, content, id }) => {
     return {
@@ -59,43 +59,3 @@ export const getMoviesReviews = async moviesId => {
     };
   });
 };
-
-// export const getTrending = async () => {
-//   const res = await axios.get(
-//     `https://api.themoviedb.org/3/trending/movie/day?api_key=${API_KEY}`
-//   );
-
-//   return res.data;
-// };
-
-// export const searchMovies = async query => {
-//   const res = await axios.get(
-//     `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=${query}&page=1&include_adult=false`
-//   );
-
-//   return res.data;
-// };
-
-// export const getMovieDetails = async id => {
-//   const res = await axios.get(
-//     `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}&language=en-US`
-//   );
-
-//   return res.data;
-// };
-
-// export const getMovieCredits = async id => {
-//   const res = await axios.get(
-//     `https://api.themoviedb.org/3/movie/${id}/credits?api_key=${API_KEY}&language=en-US`
-//   );
-
-//   return res.data.cast;
-// };
-
-// export const getMoviesReviews = async id => {
-//   const res = await axios.get(
-//     `https://api.themoviedb.org/3/movie/${id}/reviews?api_key=${API_KEY}&language=en-US&page=1`
-//   );
-
-//   return res.data;
-// };
